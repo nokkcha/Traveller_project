b22c1e096a8c9f97f184a39d93265034437c2d44
nokkcha <hover16@naver.com>
2021-09-09 15:53:05 +0900

    name show

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getAuth, signOut } from "@firebase/auth";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
@@ -9,15 +9,26 @@ const Button = styled.button`
   height: 40px;
   border-radius: 20px;
 `;
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const [user, setUser] = useState(userObj.displayName);
   const history = useHistory();
   const onClick = () => {
     const auth = getAuth();
     signOut(auth);
     history.push("/home");
   };
+
+  useEffect(() => {
+    if (user === null) {
+      setUser("Traveller");
+    } else {
+      return;
+    }
+  }, []);
+
   return (
     <>
+      <h2>안녕하세요,{user}님.</h2>
       <Button onClick={onClick}>log out</Button>
     </>
   );
